85c4f8b39ded3b240c354149cb6a38ec35612fbd
YAN <mac@macs-iMac.local>
2020-03-21 12:28:30 +0800

    first commit

---
+++ test_3.15/test_3.15/main.c
@@ -0,0 +1,36 @@
+//
+//  main.c
+//  test_3.15
+//
+//  Created by YAN on 2020/3/15.
+//  Copyright © 2020 Yeee. All rights reserved.
+//
+#include<stdio.h>
+#include<math.h>
+//int main()
+//{
+    //  int a=10;
+    //  int b=++a;
+    //  printf("b= %d a=%d\n",b,a);
+    //  强制类型转换
+    //  int a=(int)3.14;
+// int a=3;
+  //  int b=5;
+   // if(a&&b)
+     //   printf("hehe"\n);
+   // return 0;
+//}
+int main()
+{
+    int a=10;
+    int b=20;
+    int max=0;//存放找到的较大值
+    if(a>b)
+        max=a;
+    else
+        max=b;
+    printf(" max=%d\n",max);
+    
+}
+
+
