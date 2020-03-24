3312bcb3d439043d3a488c52d4414528ccd5aafa
Bo-rrr <17747081459@qq.com>
2020-03-24 12:54:52 +0800

    third commit

--- test_3_23/test_3_23/main.c
+++ test_3_23/test_3_23/main.c
@@ -49,24 +49,49 @@
 //
 //    return 0;
 //}
+//int main()
+//{
+//    int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//    int k = 0;
+//    printf("你要找哪个数字: ");
+//    scanf("%d", &k);
+//    int sz = sizeof (arr) / sizeof (arr[0]);//计算元素个数
+//    int i = 0;
+//    for (i = 1; i < sz; i++)
+//    {
+//        if (arr[i] == k)
+//        {
+//            printf("找到了 ，下标是: %d\n", i);
+//        break;
+//        }
+//    }
+//        if (i == sz)
+//        {
+//            printf("找不到");
+//        }
+//
+//    return 0;
+//}
 int main()
 {
-    int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-    int k = 7;
-    int sz = sizeof (arr) / sizeof (arr[0]);//计算元素个数
-    int i = 0;
-    for (i = 1; i < sz; i++)
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int k = 0;
+    printf("请输入要查找的数字: ");
+    scanf("%d ", &k);
+    int sz = sizeof (arr) / sizeof (arr[0]);
+    
+    int left = 0;
+    int right = sz -1;
+    int mid = (left + right) / 2;
+    
+    if (mid < k)
     {
-        if (arr[i] == k)
-        {
-            printf("找到了 ，下标是: %d\n", i);
-        break;
-        }
+        left = mid + 1;
     }
-        if (i == sz)
-        {
-        printf("找不到\n")；
-        }
+    else if (mid > k)
+    {
+        right = mid - 1;
+    }
     
     return 0;
 }
