ed7d1e4fb497672a3f3664d75ff30d6d1fdcb5f4
YAN <mac@macs-iMac.local>
2020-03-24 12:30:37 +0800

    second commit

--- test_3_23/test_3_23/main.c
+++ test_3_23/test_3_23/main.c
@@ -7,35 +7,66 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 //
 //int main(int argc, const char * argv[]) {
 //    // insert code here...
 //    printf("Hello, World!\n");
 //    return 0;
 //}
+//计算n的阶乘
 //int main()
 //{
-//    int i = 0;
 //    int n = 0;
-//    int sum = 1;
-//    scanf ("%d ", &n);
+//    scanf("%d", &n);
+//    int i = 1;
+//    int ret = 1;
+//    for (i = 1; i <= n; i++)
+//    {
+//        ret = ret * i;
+//    }
+//    printf("%d! = %d", ret);
+//    return 0;
+//}
+//int main()
+//{
+//    int n = 0;
+//    int i = 1;
+//    int sum = 0;
+//    int ret = 1;
+//
+//    for (n = 1; n <= 3 ; n++)
+//    {
+//        ret = 1;
 //        for (i = 1; i <= n; i++)
 //        {
-//            sum = i * sum;
+//            ret = ret * i;
 //        }
-//    printf ("%d! = %d", sum);
+//        sum = sum + ret;
+//    }
+//
+//    printf("%d\n", sum);
 //
 //    return 0;
 //}
-#include <stdio.h>
 int main()
 {
-    int i,n;
-    double sum=1;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-        sum=sum*i;
-    printf("%d!=%lf",n,sum);
-    printf("\n");
+    int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int k = 7;
+    int sz = sizeof (arr) / sizeof (arr[0]);//计算元素个数
+    int i = 0;
+    for (i = 1; i < sz; i++)
+    {
+        if (arr[i] == k)
+        {
+            printf("找到了 ，下标是: %d\n", i);
+        break;
+        }
+    }
+        if (i == sz)
+        {
+        printf("找不到\n")；
+        }
+    
     return 0;
 }
