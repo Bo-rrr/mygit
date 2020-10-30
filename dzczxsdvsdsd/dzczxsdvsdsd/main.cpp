70326e617b7923c6d0953ad2166798e15fd62486
1774708159@qq.com <1774708159@qq.com>
2020-10-30 18:26:27 +0800

    test.cc

---
+++ dzczxsdvsdsd/dzczxsdvsdsd/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  dzczxsdvsdsd
+//
+//  Created by hh on 2020/10/23.
+//  Copyright © 2020 Yeee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
